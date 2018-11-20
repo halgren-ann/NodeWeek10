586aecbf6bfbe11f25993acaea85b2a4a0263e2c
Ann Halgren <annabellelarsen@gmail.com>
2018-11-19 21:43:53 -0800

    Added getRate and result.ejs and changed stuff

--- index.js
+++ index.js
@@ -8,31 +8,28 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .post('/getRate', (req, res) => {
-    //var basePrice = 0;
-    //answer = null;
-    //var v1 = Number(req.query.weight);
+    var basePrice = 0;
+    answer = "";
+    var v1 = Number(req.query.weight);
     if (req.query.mailType == 'stamped') {
       basePrice = 0.50;
       answer = "Your selected mail was: 'Letters (Stamped)' \n" +
                "You specified that the wight of your package is:  " + v1 + "oz\n" +
                "The price of mailing your package is: $" + (basePrice + (0.21 * (v1-1)));
     }
-
-    if (req.query.mailType == 'metered'){
+    else if (req.query.mailType == 'metered'){
         basePrice = 0.47;
         answer = "Your selected mail was: 'Letters (Metered)' \n" +
                 "You specified that the wight of your package is:  " + v1 + "oz\n" +
                 "The price of mailing your package is: $" + (basePrice + (0.21 * (v1-1)));
     }
-
-    if (req.query.mailType == 'flats'){
+    else if (req.query.mailType == 'flats'){
         basePrice = 1.0;
-        answer = "Your selected mail was: 'Letters (Metered)' \n" +
+        answer = "Your selected mail was: 'Large Envelopes (Flats)' \n" +
             "You specified that the wight of your package is:  " + v1 + "oz\n" +
             "The price of mailing your package is: $" + (basePrice + (0.21 * (v1-1)));
     }
-
-    if (req.query.mailType == 'package'){
+    else {
 
         if(v1 >= 5){
             basePrice = 3.5;
@@ -55,7 +52,7 @@ express()
         else if(v1 = 13){
           basePrice = 5.50
         }
-        answer = "Your selected mail was: 'Letters (Metered)' \n" +
+        answer = "Your selected mail was: 'First-Class Package Service - Retail' \n" +
             "You specified that the wight of your package is:  " + v1 + "oz\n" +
             "The price of mailing your package is: $" + (basePrice);
     }
